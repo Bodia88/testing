486a7add7c1d27ac76c29f3dea7529062356ccd1
Bodia88 <92109547+Bodia88@users.noreply.github.com>
2022-04-09 13:56:00 +0300

    reset css

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 console.log('Hello!');
+
+const obj = {
+    x: 10,
+    y: 5,
+    add: function() { console.log(this.x + this.y, this) },
+    delete: function() { console.log(this.x - this.y) }
+};
+
+const obj1 = {
+    x: 7,
+    y: 2,
+};
+
+//obj.add()
+//obj.add.call(obj1);
+const func = obj.add.bind(obj1);
+func();
+//obj.add;
+
+document.addEventListener('click', (e) => console.log(this))
